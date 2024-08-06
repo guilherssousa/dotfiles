49ccf9c69ed6a4efebc3cec76e18810cd34b52bc
Guilherme Sousa <guilherssousa@gmail.com>
2024-08-06 12:06:27 -0300

    remove debug

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -26,8 +26,8 @@ require('lazy').setup({
     priority = 1000,
     config = function()
       vim.cmd.colorscheme('rose-pine')
-      -- vim.api.nvim_set_hl(0, 'Normal', { bg = "none" })
-      -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = "none" })
+      -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+      -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
     end,
   },
   -- make it transparent!
