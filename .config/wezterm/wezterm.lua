ec2b91a01b1ef11b36598358e428379fc48b3d9f
Guilherme Sousa <guilherssousa@gmail.com>
2024-08-11 20:07:08 -0300

    Update my themes

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,40 +1,48 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local config = {
-  -- # Cosmetic Options (https://wezfurlong.org/wezterm/colorschemes/)
-  enable_tab_bar = false,
+	-- # Cosmetic Options (https://wezfurlong.org/wezterm/colorschemes/)
+	enable_tab_bar = false,
 
-  -- # Font Options
-  font = wezterm.font("JetBrainsMono Nerd Font Mono"),
-  font_size = 18.0,
+	-- # Font Options
+	font = wezterm.font("JetBrainsMono Nerd Font Mono"),
+	font_size = 18.0,
 
-  -- # Background options
-  window_background_opacity = 0.93,
-  win32_system_backdrop = 'Acrylic',
+	-- # Background options
+	window_background_opacity = 0.93,
+	win32_system_backdrop = "Acrylic",
 
-  -- # Keys configuration
-  keys = {
-    {
-      key = 'n',
-      mods = 'SHIFT|CTRL',
-      action = wezterm.action.ToggleFullScreen,
-    }
-  },
+	-- # Keys configuration
+	keys = {
+		{
+			key = "n",
+			mods = "SHIFT|CTRL",
+			action = wezterm.action.ToggleFullScreen,
+		},
+	},
 
-  -- # Mouse bindings
-  mouse_bindings = {
-    -- Ctrl-click will open the link under the mouse  cursor
-    {
-      event = {
-        Up = { streak = 1, button = 'Left' }
-      },
-      mods = "CTRL",
-      action = wezterm.action.OpenLinkAtMouseCursor,
-    }
-  },
+	-- # Mouse bindings
+	mouse_bindings = {
+		-- Ctrl-click will open the link under the mouse  cursor
+		{
+			event = {
+				Up = { streak = 1, button = "Left" },
+			},
+			mods = "CTRL",
+			action = wezterm.action.OpenLinkAtMouseCursor,
+		},
+	},
+
+	-- remove window padding
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
 }
 
 -- Set those stuff after object initialization for copy-paste reasons...
-config.color_scheme = 'Solarized Dark Higher Contrast'
+config.color_scheme = "Kanagawa Dragon (Gogh)"
 
 return config
