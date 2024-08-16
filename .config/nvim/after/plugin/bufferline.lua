e8cb00c1925899037dadfee959a8f4e3d77c9b09
Guilherme Sousa <guilherssousa@gmail.com>
2024-08-16 10:08:46 -0300

    Update leader rules

--- .config/nvim/after/plugin/bufferline.lua
+++ .config/nvim/after/plugin/bufferline.lua
@@ -1,18 +1,17 @@
-require("bufferline").setup({
+require('bufferline').setup({
   options = {
-    mode = "buffers",
+    mode = 'buffers',
     diagnostics = 'nvim_lsp',
     hover = {
       enabled = true,
       delay = 200,
     },
     always_show_bufferline = false,
-    separator_style = "thin",
+    separator_style = 'thin',
   },
 })
 
 -- Map Alt+ 1-9 to switch buffers
 for i = 1, 9 do
-  vim.api.nvim_set_keymap('n', '<A-' .. i .. '>', ':BufferLineGoToBuffer ' .. i .. '<CR>',
-    { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('n', '<leader>' .. i, ':BufferLineGoToBuffer ' .. i .. '<CR>', { noremap = true, silent = true })
 end
