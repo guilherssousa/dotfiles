755f0f4c870f970fcebb645539f92bb9e18d1c3f
Guilherme Sousa <guilherssousa@gmail.com>
2024-10-06 17:37:59 -0300

    Fix Blade treesitter syntax name

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -36,7 +36,7 @@ local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
 
 parser_config.blade = {
   install_info = {
-    url = 'https://github.com/EmramMR/tree-sitter-blade',
+    url = 'https://github.com/EmranMR/tree-sitter-blade',
     files = { 'src/parser.c' },
     branch = 'main',
   },
