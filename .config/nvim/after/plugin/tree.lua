353134a8d1c1324cbea051e31fb4b7c5f3125ee0
Guilherme da Silva Sousa <guilherssousa@gmail.com>
2025-06-24 11:21:33 -0300

    update

--- .config/nvim/after/plugin/tree.lua
+++ .config/nvim/after/plugin/tree.lua
@@ -5,6 +5,18 @@ neotree.setup({
     statusline = false,
   },
   filesystem = {
+    filtered_items = {
+      visible = true,
+      show_hidden_count = true,
+      hide_dotfiles = false,
+      hide_gitignored = true,
+      hide_by_name = {
+        '.git',
+        '.DS_Store',
+        'thumbs.db',
+      },
+      never_show = {},
+    },
     window = {
       position = 'right',
       mappings = {
